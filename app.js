6296c2ec81826bebffa26b23fe8013d7cb083306
Gian <157512980+Giann77@users.noreply.github.com>
2024-09-08 10:44:12 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 // Função 'pesquisar', responsável por exibir os resultados da pesquisa na página
 function pesquisar() {
-    
-
     // Obtém o elemento HTML com o ID 'resultados-pesquisa', onde os resultados serão exibidos
     let section = document.getElementById("resultados-pesquisa");
     let campoPesquisa = document.getElementById("campo-pesquisa").value;
     let naoEncontrado = document.getElementById("Nada foi encontrado");
 
+    // Limpa o conteúdo anterior dos elementos de resultados e "nada foi encontrado"
     section.innerHTML = "";
     naoEncontrado.innerHTML = "";
 
@@ -16,51 +15,56 @@ function pesquisar() {
     let descricao = "";
     let tags = "";
 
+    // Verifica se o campo de pesquisa está vazio
+    if (campoPesquisa == ("")) {
+        naoEncontrado.innerHTML = ""; // Limpa o texto de "nada foi encontrado" se o campo estiver vazio
+        return; // Encerra a função se não houver texto de pesquisa
+    }
 
+    // Converte o texto de pesquisa para minúsculas para comparação
     campoPesquisa = campoPesquisa.toLowerCase();
 
     // Loop 'for...of' para percorrer cada item dentro da variável 'dados' (presumivelmente um array de objetos)
     for (let dado of dados) {
-
+        // Obtém e transforma as propriedades do dado para minúsculas
         tags = dado.tags.toLowerCase();
         titulo = dado.titulo.toLowerCase();
         descricao = dado.descricao.toLowerCase();
         
-        if (titulo.includes(campoPesquisa)||descricao.includes(campoPesquisa) || tags.includes(campoPesquisa))
-                  {
-
+        // Verifica se o campo de pesquisa está presente no título, descrição ou tags do dado
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
             // Concatena em 'resultados' uma estrutura HTML para cada item da pesquisa
             resultados += `
-        <div class="item-resultado"> 
-        <h2>
-            <a href="#" target="_blank">${dado.titulo}</a> <!-- Exibe o título do dado com um link -->
-        </h2>
-        <p>${dado.nascimento}</p> <!-- Exibe a data de nascimento do dado -->
-        <p class="descricao-meta">${dado.descricao}</p> <!-- Exibe a descrição do dado -->
-        <p>${dado.ator}</p> <!-- Exibe o ator relacionado ao dado -->
-        <a href="${dado.link}" class="link-colorido" target="_blank">Mais informações</a> <!-- Exibe um link para mais informações -->
-    </div>
-      `
+            <div class="item-resultado"> 
+                <h2>
+                    <a href="#" target="_blank">${dado.titulo}</a> <!-- Exibe o título do dado com um link -->
+                </h2>
+                <p>${dado.nascimento}</p> <!-- Exibe a data de nascimento do dado -->
+                <p class="descricao-meta">${dado.descricao}</p> <!-- Exibe a descrição do dado -->
+                <p>${dado.ator}</p> <!-- Exibe o ator relacionado ao dado -->
+                <a href="${dado.link}" class="link-colorido" target="_blank">Mais informações</a> <!-- Exibe um link para mais informações -->
+            </div>
+            `;
         }
     }
 
+    // Se não houver resultados, exibe uma mensagem indicando que nada foi encontrado
     if (!resultados) {
-        naoEncontrado.innerHTML = "<p>Nada foi encontrado</p>"
+        naoEncontrado.innerHTML = "<p>Nada foi encontrado</p>";
     }
 
     // Insere o conteúdo HTML gerado na variável 'resultados' dentro do elemento 'section'
     section.innerHTML = resultados;
 }
 
+// Função para tratar eventos de tecla
 function tratarEventoTecla(event) {
-    if ( event.key === "Enter") { // Verifica se a tecla pressionada é Enter
+    if (event.key === "Enter") { // Verifica se a tecla pressionada é Enter
+        pesquisar(); // Chama a função de pesquisa
     }
 }
 
-
-
 // Adiciona ouvintes de eventos
 document.getElementById("campo-pesquisa").addEventListener("change", pesquisar); // Adiciona evento para quando o campo mudar
-document.getElementById("campo-pesquisa").addEventListener("input", pesquisar);
-
-
+document.getElementById("campo-pesquisa").addEventListener("input", pesquisar); // Adiciona evento para quando o valor do campo for alterado
+document.getElementById("campo-pesquisa").addEventListener("keydown", tratarEventoTecla); // Adiciona evento para detectar pressionamento de teclas
